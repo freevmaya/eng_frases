c1ec262b521025ad39848ef6d750633bfbce4834
Vadim Frolov <@vmaya>
2026-01-24 15:06:53 +0500

    requlary

--- src/Vmaya/templates/home.php
+++ src/Vmaya/templates/home.php
@@ -1,5 +1,5 @@
 <?php
-    $v = 64;
+    $v = 65;
 ?>
 <!-- Settings Modal -->
 <div class="modal fade fullscreen-modal" id="settingsModal" tabindex="-1" aria-labelledby="settingsModalLabel" aria-hidden="true">
--- src/Vmaya/templates/index.php
+++ src/Vmaya/templates/index.php
@@ -1,5 +1,5 @@
 <?php
-	$v = 64;
+	$v = 65;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
