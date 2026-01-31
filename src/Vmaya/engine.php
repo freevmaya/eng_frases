7ce2aef98f9746ddcb32349595def05ce012fc41
Vadim Frolov <@vmaya>
2026-01-31 11:34:58 +0500

    requlary

--- src/Vmaya/engine.php
+++ src/Vmaya/engine.php
@@ -9,7 +9,7 @@
 	define('TEMPLATES_PATH', BASEPATH.'/src/Vmaya/templates/');
 	define("CHARSET", "utf-8");
 	define("DEFAULTPAGE", "home");
-	define("SCRIPTS_VERSION", 129);
+	define("SCRIPTS_VERSION", 130);
 	
 	$lv = SCRIPTS_VERSION % 1000;
 	$mv = floor(SCRIPTS_VERSION / 1000);
