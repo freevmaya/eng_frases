48937c1238d51db0ca9b64088447594e684f3365
Vadim Frolov <@vmaya>
2026-02-01 18:30:46 +0500

    requlary

--- public/index.php
+++ public/index.php
@@ -1,14 +1,18 @@
 <?
-	/*
-	// HttpOnly и Secure куки
+	ini_set('session.cookie_samesite', 'None');
+	ini_set('session.cookie_secure', 1);
+	ini_set('session.cookie_httponly', 1);
+	ini_set('session.use_only_cookies', 1);
+
+	// Для PHP 7.3+ можно использовать session_set_cookie_params
 	session_set_cookie_params([
-	    'lifetime' => 3600,
+	    'lifetime' => 0,
 	    'path' => '/',
-	    'domain' => $_SERVER['HTTP_HOST'],
-	    'secure' => true,     // Только HTTPS
-	    'httponly' => true,   // Недоступны из JS
-	    'samesite' => 'Strict' // Защита от CSRF
-	]);*/
+	    'domain' => $_SERVER['HTTP_HOST'] ?? '',
+	    'secure' => true,
+	    'httponly' => true,
+	    'samesite' => 'None'
+	]);
 
 	session_start();
 	require dirname(__FILE__, 2).'/src/Vmaya/engine.php';
