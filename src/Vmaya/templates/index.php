27b250de4e7bfe8df74157e55bb0a31d229f35bd
Vadim Frolov <@vmaya>
2026-02-01 18:13:11 +0500

    requlary

--- src/Vmaya/templates/index.php
+++ src/Vmaya/templates/index.php
@@ -11,6 +11,9 @@
 
     <!-- PWA Support -->
     <link rel="manifest" href="manifest.json">
+    <script type="text/javascript">
+    	let user_id = <?= Page::getSession('user_id', 0)?>;
+    </script>
     
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
