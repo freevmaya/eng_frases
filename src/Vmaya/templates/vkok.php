e4f789616130c4645552938b5b043b28aa5c99c9
Vadim Frolov <@vmaya>
2026-02-06 09:12:28 +0500

    requlary

--- src/Vmaya/templates/vkok.php
+++ src/Vmaya/templates/vkok.php
@@ -44,6 +44,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?include('noindex.php')?>
     <title><?php echo APP_NAME; ?></title>
 
     <!-- PWA Support -->
