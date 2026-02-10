212e73c520b52d7b18a1c761433d3bd6a8a9e2a1
Vadim Frolov <@vmaya>
2026-02-10 15:04:35 +0500

    requlary

--- english-phrases-server/app.py
+++ english-phrases-server/app.py
@@ -293,7 +293,3 @@ if __name__ == '__main__':
     else:
         # Запуск вручную (для отладки)
         app.run(host='0.0.0.0', port=5002, debug=True)
-
-else:
-    # Если импортируется как модуль (например, через gunicorn)
-    init_database()
