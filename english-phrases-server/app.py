81a135a726b718d6c72d7eba30bd68225538201c
Vadim Frolov <@vmaya>
2026-02-13 09:10:16 +0500

    requlary

--- english-phrases-server/app.py
+++ english-phrases-server/app.py
@@ -196,6 +196,7 @@ def generate_phrases_from_ai(native_lang, target_lang, theme, count=10):
     # Заменяем плейсхолдеры в промте
     prompt = prompt_template.format(count=count, theme=theme)
     
+    print(prompt)
     # Вызываем ИИ
     response = generate_phrases(prompt)
     
