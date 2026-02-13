213d1449f2c48d6225bb5f7d26417a95486999b3
Vadim Frolov <@vmaya>
2026-02-13 19:22:54 +0500

    requlary

--- src/Vmaya/templates/index.php
+++ src/Vmaya/templates/index.php
@@ -38,7 +38,7 @@
     <!-- Bootstrap & jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
+    <script src="scripts/crypto-js.min.js"></script>
 
     <script src="scripts/component.js<?=$v?>"></script>
     <script src="scripts/main.js<?=$v?>"></script>
