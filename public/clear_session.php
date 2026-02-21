139afe843680062250bc90ccc8117755e63bccd4
Vadim Frolov <@vmaya>
2026-02-21 20:07:33 +0500

    requlary

--- public/clear_session.php
+++ public/clear_session.php
@@ -1,3 +1,14 @@
 <?
 	require dirname(__FILE__, 2).'/src/Vmaya/engine.php';
 	$_SESSION = [];
+
+?>
+<html>
+<head>
+	<script type="text/javascript">
+		localStorage.clear();
+		console.log('localStorage.clear()');
+	</script>
+</head>
+Clear ALL!
+</html>
