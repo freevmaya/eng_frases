116b480b053a109e8cd5a70cf4779608139e38d2
Vadim Frolov <@vmaya>
2026-03-18 08:59:13 +0500

    requlary

--- src/Vmaya/engine.php
+++ src/Vmaya/engine.php
@@ -10,7 +10,7 @@
 	define("CHARSET", "utf-8");
 	define("DEFAULTPAGE", "home". (DEV ? '' : ''));
 	define("DEFAULTCLASS", "Main");
-	define("SCRIPTS_VERSION", 295);
+	define("SCRIPTS_VERSION", 296);
 	
 	define("SOURCES", ['vk', 'ok', 'site', 'tg', 'e-vk', 'e-ok', 'e-tg']);
 	define("LIVETOKEN", DEV ? 20 : 60 * 10); // Врямя жизни токена 10 мин.
