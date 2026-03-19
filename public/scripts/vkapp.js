9d7b1c96a76e0b6218fe42281d18d9ed81d8cc78
Vadim Frolov <@vmaya>
2026-03-19 08:41:24 +0500

    requlary

--- public/scripts/vkapp.js
+++ public/scripts/vkapp.js
@@ -176,7 +176,8 @@ class VKApp {
 					  	// Приложение установлено в сообщество
 						stateManager.state.sharedInCommunity = data.group_id;
 						stateManager.saveState();
-					} else tracer.log(data);
+					}
+					tracer.log(data);
 				})
 				.catch((error) => {
 					// Ошибка
